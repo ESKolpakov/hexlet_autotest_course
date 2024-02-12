654d28942ea0a82110b73168d005920810ebec5d
Evgeniy Kolpakov <eskolpakov@gmail.com>
2024-02-12 23:04:54 +0300

    fixed capitalize.py

--- package_name/capitalize.py
+++ package_name/capitalize.py
@@ -1,6 +1,14 @@
 def capitalize(text):
+    """capitalize text
+    >>> capitalize('hexlet')
+    'Hexlet'
+    """
     if text == '':
         return ''
     first_char = text[0].upper()
     rest_substring = text[1:]
     return f'{first_char}{rest_substring}'
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
